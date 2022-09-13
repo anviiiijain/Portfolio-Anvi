04c6695e74b0cd742b0348528ebe909953bf053c
Anvi Jain <60340375+anviiiijain@users.noreply.github.com>
2022-09-13 15:16:55 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/anviiiijain/portfolio/master/src/Assets/Anvi_Jain_Resume.pdf";
+  "https://raw.githubusercontent.com/anviiiijain/Portfolio-Anvi/master/src/Assets/Anvi_Jain_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
