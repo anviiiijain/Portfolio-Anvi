d80748b96c98b5a52c049da6d953c6c48193aa5a
anviiiijain <anvijain250102@gmail.com>
2022-12-28 00:54:35 +0530

    fixed github block size

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -11,7 +11,7 @@ function Github() {
       <div style={{ width: '100%' }}>
         <GitHubCalendar
           username='anviiiijain'
-          blockSize={10}
+          blockSize={15}
           blockMargin={5}
           color='#c084f5'
           fontSize={16}
