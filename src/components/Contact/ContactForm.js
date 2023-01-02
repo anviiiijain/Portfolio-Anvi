b7a112eadebb48952818550efbbc75a23ce7b4ba
anviiiijain <anvijain250102@gmail.com>
2023-01-02 23:54:28 +0530

    contact form updated

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,22 +1,76 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Button, Card } from 'react-bootstrap'
 import { AiFillGithub, AiFillInstagram } from 'react-icons/ai'
 import { FaLinkedinIn } from 'react-icons/fa'
+import emailjs from '@emailjs/browser'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    message: '',
+  })
+  const handleChange = (e) => {
+    let { value, name } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_sss5x4w',
+        'template_4ytdxax',
+        form.current,
+        'XWzxOno_MjgjTLJtd'
+      )
+      .then(
+        (result) => {
+          toast.success('Email Sent', {
+            position: 'top-right',
+            hideProgressBar: true,
+            closeOnClick: true,
+            theme: 'colored',
+          })
+        },
+        (error) => {
+          toast.error('Error occured', {
+            position: 'top-right',
+            hideProgressBar: true,
+            closeOnClick: true,
+            theme: 'colored',
+          })
+        }
+      )
+      .finally(() => {
+        setFormData({
+          user_name: '',
+          user_email: '',
+          message: '',
+        })
+      })
+  }
   return (
     <section className='contact-section'>
       <h1>Contact Me</h1>
       <Card className='contact-card'>
         <Card.Body>
-          <form></form>
-          <form encType='multipart/form-data'>
+          <form
+            ref={form}
+            onSubmit={(e) => sendEmail(e)}
+            encType='multipart/form-data'
+          >
             <div className='form-group'>
               <label className='mb-1 mt-3 form-label'>
                 Your name<span className='text-danger'>*</span>
               </label>
               <input
-                name='name'
+                required
+                name='user_name'
+                value={formData.user_name}
+                onChange={(e) => handleChange(e)}
                 type='text'
                 className='form-control'
                 placeholder='Name'
@@ -27,7 +81,10 @@ function ContactForm() {
                 Your email<span className='text-danger'>*</span>
               </label>
               <input
-                name='email'
+                required
+                name='user_email'
+                value={formData.user_email}
+                onChange={(e) => handleChange(e)}
                 type='email'
                 className='form-control'
                 placeholder='Email'
@@ -38,7 +95,10 @@ function ContactForm() {
                 Message<span className='text-danger'>*</span>
               </label>
               <textarea
+                required
                 name='message'
+                value={formData.message}
+                onChange={(e) => handleChange(e)}
                 type='text'
                 className='form-control'
                 placeholder='Message'
@@ -86,6 +146,7 @@ function ContactForm() {
           </a>
         </li>
       </ul>
+      <ToastContainer />
     </section>
   )
 }
